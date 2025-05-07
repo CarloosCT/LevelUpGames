90ea726715898a9f04248b4f7d702a766ec83603
ErChcechi <cesar2005.cq@gmail.com>
2025-05-07 08:41:14 +0200

    Modificacion de homeController

--- levelupgamesTFG/src/main/java/com/tfg/levelupgames/controller/web/HomeController.java
+++ levelupgamesTFG/src/main/java/com/tfg/levelupgames/controller/web/HomeController.java
@@ -27,24 +27,4 @@ public class HomeController {
         m.put("estilos", "/css/home/style.css");
         return "_t/frame";
     }
-
-    @GetMapping("/test")
-    public void test(
-    ) throws Exception {
-        throw new DangerException("¡¡¡¡ PUM !!!!!");
-    }
-
-    @GetMapping("/contador")
-    public String contador(
-        HttpSession s,
-        ModelMap m
-    ) {
-        if ( s.getAttribute("nVisitas") == null  )   {
-            s.setAttribute("nVisitas", 1);
-        }
-        m.put("visitas",s.getAttribute("nVisitas"));
-        
-        s.setAttribute("nVisitas", ((Integer)s.getAttribute("nVisitas"))+1);
-        return "home/contador";
-    }
 }
